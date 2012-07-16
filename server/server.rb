25ede109a909462a6389dcc35cb27d70cc218444
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-07-16 09:36:35 +0200

    department template view stub

--- server/server.rb
+++ server/server.rb
@@ -3,23 +3,26 @@ require "goliath"
 require 'goliath/rack/templates'
 require "slim"
 require "./models"
+require "cgi"
 
 dbconfig = YAML::load(File.open("config/database.yml"))
 ActiveRecord::Base.establish_connection(dbconfig[Goliath.env.to_s])
 
 class Server < Goliath::API
   include Goliath::Rack::Templates
-  @@org = Organization.first
-
+  #use Goliath::Rack::Validation::RequestMethod, %w(GET)
   use(Rack::Static,
       :root => Goliath::Application.app_path('public'),
       :urls => ['/favicon.ico', '/css', '/js', '/img'])
+
+  # get access to organization object
+  @@org = Organization.first
 
   def response(env)
     # (not to elegant) URL-routing:
     # I will try to implement this in Grape, as soon as I find out how
     # to serve views in Grape
-    path = env['PATH_INFO'].split('/')
+    path = CGI.unescape(env['PATH_INFO']).split('/')
     case path.length
     when 0    # matches /
       [200, {}, slim(:index, :locals => {:org => @@org})]
@@ -34,8 +37,8 @@ class Server < Goliath::API
       end
     when 3    # matches /branch/department
       if @@org.branches.find_by_name(path[1])
-        if @@org.branches.find_by_name(path[1]).departments.find_by_name(path[2])
-        [200, {}, "avdeling"]
+        if (dept = @@org.branches.find_by_name(path[1]).departments.find_by_name(path[2]))
+        [200, {}, slim(:department, :locals => {:department => dept})]
         else  # matches non-existing department
           raise Goliath::Validation::NotFoundError
         end
