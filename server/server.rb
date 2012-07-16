823fbe6e029b4163540bbb689b32f634323dfc07
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-07-16 16:00:03 +0200

    Finishes deparment user interface Most of the jQuery interaction done. No saving of changes yet!

--- server/server.rb
+++ server/server.rb
@@ -21,7 +21,7 @@ class Server < Goliath::API
   def response(env)
     # (not to elegant) URL-routing:
     # I will try to implement this in Grape, as soon as I find out how
-    # to serve views in Grape
+    # to serve template views from Grape endpoints
     path = CGI.unescape(env['PATH_INFO']).split('/')
     case path.length
     when 0    # matches /
@@ -37,6 +37,7 @@ class Server < Goliath::API
       end
     when 3    # matches /branch/department
       if @@org.branches.find_by_name(path[1])
+        # TODO: tidy up this
         if (dept = @@org.branches.find_by_name(path[1]).departments.find_by_name(path[2]))
         [200, {}, slim(:department, :locals => {:department => dept})]
         else  # matches non-existing department
