3c26dfaef28ec9d8ff769dc0f09e2584c2d5bc13
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-07-29 21:06:32 +0200

    Finishes clients part of the API

--- server/api.rb
+++ server/api.rb
@@ -4,31 +4,43 @@ class API < Grape::API
   prefix 'api'
   format :json
 
-  resource 'organization' do
-  end
-
-  resource 'branches' do
-  end
-
-  resource 'departmentes' do
-  end
-
-  resource 'clients' do
+  resource :clients do
+    desc "returns all clients"
     get "/" do
       {:clients => Client.all }
     end
 
+    desc "returns individual client"
     get "/:id" do
-      {:client => Client.find(params[:id])}
+     {:client => Client.find(params[:id])}
     end
-  end
 
-  resource 'users' do
-  end
-
-  namespace 'options' do
-    resource 'opening_hours' do
+    desc "creates a new client and returns it"
+    post "/" do
+      new_client = Client.create(:name => params['name'], :hwaddr => params['hwaddr'],
+                   :ipaddr => params['ipaddr'])
+      if new_client.valid?
+        {:client => new_client}
+      else
+        throw :error, :status => 400, :message => "missing parameters"
+      end
     end
+
+    desc "updates an existing client and returns the updated version"
+    put "/:id" do
+      client = Client.find(params[:id])
+      updates = {}
+      params.each do |k,v|
+        updates[k] = v if (client.has_attribute?(k) and client.attributes[k].to_s != v)
+      end
+      if updates.size > 0
+        client.update_attributes(updates)
+        {:client => client}
+      else
+        throw :error, :status => 400, :message => "no changes to be made"
+      end
+    end
+
   end
 
 end
--- server/test/test_api.rb
+++ server/test/test_api.rb
@@ -26,10 +26,11 @@ describe API do
       it "lists all the clients" do
         get "/api/clients"
         last_response.status.must_equal 200
-        JSON.parse(last_response.body)["clients"].length.must_equal 2
-        JSON.parse(last_response.body)["clients"].must_be_instance_of Array
-        JSON.parse(last_response.body)["clients"][0]['name'].must_equal 'client1'
-        JSON.parse(last_response.body)["clients"][1]['name'].must_equal 'client2'
+        response = JSON.parse(last_response.body)
+        response["clients"].length.must_equal 2
+        response["clients"].must_be_instance_of Array
+        response["clients"][0]['name'].must_equal 'client1'
+        response["clients"][1]['name'].must_equal 'client2'
       end
 
       it "can access individual clients" do
@@ -43,15 +44,61 @@ describe API do
         get "/api/client/3"
         last_response.status.must_equal 404
       end
-
     end
 
-    # describe 'POST /api/clients' do
-    #   it "creates a new client" do
-    #     post "/api/clients"
-    #     last_response.status.must_equal 201
-    #   end
-    # end
+    describe 'POST /api/clients' do
+      it "creates a new client" do
+        num_of_clients_before_post = Client.all.count
+        post "/api/clients", :name => any_string, :ipaddr => any_string, :hwaddr => any_string
+        last_response.status.must_equal 201
+        Client.all.count.must_equal num_of_clients_before_post + 1
+      end
+
+      it "doesn't create a new client if required parameters are missing" do
+        num_of_clients_before_post = Client.all.count
+        post "/api/clients", :name => any_string
+        last_response.status.must_equal 400
+        Client.all.count.must_equal num_of_clients_before_post
+      end
+
+      it "returns the created client" do
+        post "/api/clients", :name => 'nju_client', :ipaddr => any_string, :hwaddr => any_string
+        last_response.status.must_equal 201
+        JSON.parse(last_response.body)['client']['name'].must_equal "nju_client"
+      end
+    end
+
+    describe 'PUT /api/clients/:id' do
+
+      before do
+        @client_updatable = Client.create(:name => any_string, :ipaddr => 'ip.1', :hwaddr => any_string)
+      end
+
+      after do
+        @client_updatable.destroy
+      end
+
+      it "updates a client with new values suplied by parameters" do
+        @client_updatable.ipaddr.must_equal 'ip.1'
+        put "/api/clients/#{@client_updatable.id}", :ipaddr => 'ip.2'
+        last_response.status.must_equal 200
+        @client_updatable.reload
+        @client_updatable.ipaddr.must_equal 'ip.2'
+      end
+
+      it "returns the updated client" do
+        name_before_update = @client_updatable.name
+        put "/api/clients/#{@client_updatable.id}", :name => "new_name"
+        last_response.status.must_equal 200
+        JSON.parse(last_response.body)['client']['name'].must_equal "new_name"
+      end
+
+      it "updates nothing if no attributes are changed" do
+        put "/api/clients/#{@client_updatable.id}", :name => @client_updatable.name
+        last_response.status.must_equal 400
+        last_response.body.must_match /no changes/
+      end
+    end
 
   end
 end
