00768e2d11a372db56508a350705d2562819d45a
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-01-07 04:03:30 +0100

    don't log admin passwords

--- server/api_server.rb
+++ server/api_server.rb
@@ -15,6 +15,7 @@ Slim::Engine.set_default_options :pretty => true
 Goliath::Request.log_block = proc do |env, response, elapsed_time|
   params = env[Goliath::Request::RACK_INPUT].string+env[Goliath::Request::QUERY_STRING]
   params = '**hidden**' if env[Goliath::Request::REQUEST_PATH].match(/authenticate/)
+  params = '**hidden**' if env[Goliath::Request::REQUEST_PATH].match(/admins/)
 
   # Logging format:
   # response.status | request.IP | request.method | request.path | response.length(bytes) | response.time(ms)
