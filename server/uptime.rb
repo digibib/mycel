f1ee7c24a9fba65ecbd9c688b5fe4f031dfbb851
hugo <hugo.osterberg@kul.oslo.kommune.no>
2017-08-08 15:10:34 +0200

    Add experimental uptime logging

---
+++ server/uptime.rb
@@ -0,0 +1,10 @@
+require './models'
+require 'logger'
+
+ActiveRecord::Base.establish_connection(Settings::DB[:production])
+logger = Logger.new('./logs/uptime.log')
+logger.datetime_format = "%Y-%m-%d %H:%M"
+
+Client.all.each do |client|
+  logger.info "id: #{client.id} status: #{client.status}"
+end
