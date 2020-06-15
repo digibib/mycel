f940c782116934dceed4b99e6b5555b6d4658e5e
bensinober <bensinober@gmail.com>
2020-06-15 22:49:19 +0200

    Add settings.rb configurable with ENV

---
+++ server/config/settings.rb
@@ -0,0 +1,27 @@
+module Settings
+  DEFAULT_MINUTES = 60
+  API = {:host => "mycel-api", :port => 9000 }
+  WS = {:host => "mycel-server", :port => 9001 }
+  SIP2 = {:host => ENV["SIP_HOST"], :port => ENV["SIP_PORT"]}
+  DB = {:test => {:adapter => "sqlite3",
+                  :database => ":memory:"},
+        :development => {:adapter => "em_mysql2",
+                         :database => "mycel",
+                         :username => ENV["USERNAME"],
+                         :password => ENV["PASSWORD"],
+                         :host => "mycel-db",
+                         :port => 3306,
+                         :pool => 1,
+                         :reconnect => false},
+        :production => {:adapter => "em_mysql2",
+                         :database => "mycel",
+                         :username => ENV["USERNAME"],
+                         :password => ENV["PASSWORD"],
+                         :host => "mycel-db",
+                         :port => 3306,
+                         :pool => 5,
+                         :reconnect => true}}
+
+  # Statistics settings
+  FILTER = ["Digitalt Bibliotek"] # Exclude test clients from stats
+end
