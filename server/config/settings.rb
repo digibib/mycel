5ee6f25c0f33cf1054888494a3059a4f1f206867
bensinober <bensinober@gmail.com>
2020-06-17 12:27:15 +0200

    Add env for SIP user and pass

--- server/config/settings.rb
+++ server/config/settings.rb
@@ -2,7 +2,7 @@ module Settings
   DEFAULT_MINUTES = 60
   API = {:host => "mycel-api", :port => 9000 }
   WS = {:host => "mycel-server", :port => 9001 }
-  SIP2 = {:host => ENV["SIP_HOST"], :port => ENV["SIP_PORT"]}
+  SIP2 = {:host => ENV["SIP_HOST"], :port => ENV["SIP_PORT"], :username => ENV["SIP_USER"], :password => ENV["SIP_PASS"]}
   DB = {:test => {:adapter => "sqlite3",
                   :database => ":memory:"},
         :development => {:adapter => "em_mysql2",
